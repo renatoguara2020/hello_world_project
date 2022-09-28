fe1ece2a54dcba551e949e03f27432d858bf98c9
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-28 09:02:11 -0300

    commit changes

---
+++ hello_world_project/validacao.py
@@ -0,0 +1,12 @@
+
+print(type(1))
+print(type(0.0))
+print(type(''))
+print(type([]))
+print(type({}))
+print(type(100 == 100))
+
+
+print(type(10 == 10 and 100 == 100))
+print(type(100 == 100 or 10 == 10))
+print(type(0))
