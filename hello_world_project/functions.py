ead34efedd5f4b1fbb258a5c73d60056f4ca18f6
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-28 12:32:36 -0300

    ok

--- hello_world_project/functions.py
+++ hello_world_project/functions.py
@@ -4,7 +4,7 @@ idade = 4
 
 
 def saudarPessoa(nome, idade):
-    print(f'Olá {nome} tudo bem sua idade é {idade} anos')
+    print(f'Olá {nome} tudo bem!!! sua idade é {idade} anos')
 
 
 print(saudarPessoa("RENATO ALVES SOARES", 51))
