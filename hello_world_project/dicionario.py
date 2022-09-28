14abacc55e5bb41ed7766782ba0525e0a9b5eed1
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-28 08:47:54 -0300

    commit changes

---
+++ hello_world_project/dicionario.py
@@ -0,0 +1,6 @@
+
+pc = {'CPU': 'Intel', 'RAM': '8gb', 'SSD': '250bg'}
+
+for chave in pc.keys():
+
+    print(f'Chave = {chave} e valor = {pc[chave]}')
---
+++ hello_world_project/tuplas.py
@@ -0,0 +1,8 @@
+
+tupla_nomes = ('Maria', 'Paulo', 'Maria', 'João', 'Sérgio', 'Pedro', 'Sérgio')
+
+print(tupla_nomes.count('Maria'))
+
+print(tupla_nomes.count('Sérgio'))
+
+print(tupla_nomes.count('Pedro'))
