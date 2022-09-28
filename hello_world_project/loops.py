da454facb8281dbbb0fc634b2194cfd59ee14103
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-28 08:32:58 -0300

    commit changes

---
+++ hello_world_project/loops.py
@@ -0,0 +1,9 @@
+lista_linguagem = ['PYTHON', 'PHP', 'JAVASCRIPT', 'PERL', 'RUBBY RAILS']
+# i = 0
+# while (i < len(lista_linguagem)):
+#     print(lista_linguagem[i])
+#     i += 1
+
+
+for lista in lista_linguagem:
+    print(lista)
---
+++ my_app/listas2.py
@@ -0,0 +1,6 @@
+adj = [10, 20, 30]
+fruits = [2, 4, 6]
+
+for x in adj:
+    for y in fruits:
+        print(x * y)
